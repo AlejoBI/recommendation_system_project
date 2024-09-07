5ebd53efdf5c516a2de4bba046cf6f8f2b0af623
Alejandro Bravo Isajar <136643710+AlejoBI@users.noreply.github.com>
2024-09-07 02:40:33 -0500

    Update CouchDB_Crud_Recommendation_System.py

--- couch_db/recommendation_system/CouchDB_Crud_Recommendation_System.py
+++ couch_db/recommendation_system/CouchDB_Crud_Recommendation_System.py
@@ -8,7 +8,7 @@ import couchdb
 #---------------------------------------------------------------
 
 # Conectar a la base de datos CouchDB
-conn_string = "http://AlejandroBI:ABI5846s@localhost:5984"
+conn_string = "URLBD"
 server = couchdb.Server(conn_string)
 db_name = "recommendation_system"
 db = server[db_name]
